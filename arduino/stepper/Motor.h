727a29c1239f3597bbe6a2231ebd8b58cadc01e2
grupafoton <61761160+grupafoton@users.noreply.github.com>
2020-03-10 11:59:40 +0100

    Create Motor.h

---
+++ arduino/stepper/Motor.h
@@ -0,0 +1,30 @@
+/*
+  Motor.h - Library for controlling stepper motor.
+  Created by photongroup, March 10, 2020.
+  Released into the public domain.
+*/
+
+#ifndef Motor_h
+#define Motor_h
+
+#include "Arduino.h"
+
+class Motor
+{
+  public:
+    Motor(int directionPin, int stepPin, float angularResolution, float delayPerStep);
+    void setScreenDistance(float screenDistance);
+    void setDirection(bool clockwise);
+    void moveNSteps (int numberOfSteps);
+    void moveAngle(float angle);
+    void movePermanently();
+    void moveSpot(float xDisplacement, float yDisplacement);
+  private:
+    int _directionPin;
+    int _stepPin;
+    float _angularResolution;
+    float _delayPerStep;
+};
+
+#endif
+
