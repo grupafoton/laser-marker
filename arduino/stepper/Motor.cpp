64bf55dccbf457ac888c41cc548ba4251d694682
grupafoton <61761160+grupafoton@users.noreply.github.com>
2020-03-10 12:14:58 +0100

    Create Motor.cpp

---
+++ arduino/stepper/Motor.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "Motor.h"
+
+Motor::Motor(int directionPin, int stepPin, float angularResolution, float delayPerStep)
+{
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  
+  _directionPin = directionPin;
+  _stepPin = stepPin;
+  _angularResolution = angularResolution;
+  _delayPerStep = delayPerStep;
+}
+
+void Motor::setScreenDistance(float screenDistance)
+{
+  _screenDistance = screenDistance;
+}
+
+void Motor::moveNSteps(float alfa)
+{
+  int numberOfSteps = alfa/_angularResolution; //approximate steps number to achieve desired angle
+  MoveNSteps(numberOfSteps);
+}
+
+void Motor::moveAngle(float alfa)
+{
+  int numberOfSteps = alfa/_angularResolution; //approximate steps number to achieve desired angle
+  MoveNSteps(numberOfSteps);
+}
+
+//Move spot on the screen by given distance
+//One stepper motor controls displacement in one direction
+void Motor::moveSpot(float displacement)
+{
+  float alfa = displacement/screenDistance; // alfa = tg(alfa) for distant screen
+  MoveAngle(alfa);
+}
