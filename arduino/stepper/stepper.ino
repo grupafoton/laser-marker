6b2f66a7f29239ef775df8440d02e9376395c781
mars-low <53299913+mars-low@users.noreply.github.com>
2020-03-11 14:39:57 +0100

    Update stepper.ino for Motor library

--- arduino/stepper/stepper.ino
+++ arduino/stepper/stepper.ino
@@ -1,47 +1,39 @@
 /*
- Stepper Motor Control - speed control
-
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
- A potentiometer is connected to analog input 0.
-
- The motor will rotate in a clockwise direction. The higher the potentiometer value,
- the faster the motor speed. Because setSpeed() sets the delay between steps,
- you may notice the motor is less responsive to changes in the sensor value at
- low speeds.
-
- Created 30 Nov. 2009
- Modified 28 Oct 2010
- by Tom Igoe
-
+ Stepper Motor Control
  */
 
-#include <Stepper.h>
+#include <Motor.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
+// parameters for motor
+const float angularResolution = 1.8;  // change this to fit the number of steps per revolution
+const float delayPerStep = 1; // time it takes to rotate by atomic angle??
+const float screenDistance = 1.5; // distance to the wall = 1.5 m
 
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+// initialize the stepper library on pins 8 and 9:
+Motor myMotor1(8, 9, angularResolution, delayPerStep);
+Motor myMotor2(10, 11, angularResolution, delayPerStep);
 
 int stepCount = 0;  // number of steps the motor has taken
 
+bool clockwise = true; // rotate motor clockwise
+bool writeEnable = false; // switch off laser light, toggle to switch on
+
 void setup() {
-  // nothing to do inside the setup
+ myMotor1.setScreenDistance(screenDistance);
+ myMotor2.setScreenDistance(screenDistance);
 }
 
 void loop() {
-  // read the sensor value:
-  int sensorReading = analogRead(A0);
-  // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
-  // set the motor speed:
-  if (motorSpeed > 0) {
-    myStepper.setSpeed(motorSpeed);
-    // step 1/100 of a revolution:
-    myStepper.step(stepsPerRevolution / 100);
+  // rotate motors to move laser point on the wall
+  if (writeEnable == true) {
+    myStepper1.moveAngle(5);
+    myStepper2.moveAngle(1.8);
   }
+  else
+  {
+   myStepper1.moveNSteps(10); // move motor to show it works
+  }
+ 
 }
 
 
